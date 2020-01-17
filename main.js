2adda2e940ac0091d4b3f5dff861fdfd02c208fc
Dhrumil Mehta <dhrumil.mehta@gmail.com>
2020-01-17 05:15:54 -0500

    add code to display posts

--- main.js
+++ main.js
@@ -16,3 +16,24 @@ function submit() {
 		return response.text()
 	}).then(alert)
 }
+
+function writePosts() {
+	let content = document.querySelector('textarea').value;
+	let payload = {
+		"title": "New Post",
+		"content": content
+	}
+
+	fetch("https://thawing-castle-69213.herokuapp.com/api/v1/posts/", {
+		method: 'GET',
+	}).then(function(response){
+		return response.json()
+	}).then(function(posts){
+
+		posts.reverse().forEach(post => {
+			document.getElementById('post-container').innerHTML += '<h1>' + post.title + '</h1>';
+			document.getElementById('post-container').innerHTML += '<p>' + post.content + '</p>';
+		});
+
+	})
+}
